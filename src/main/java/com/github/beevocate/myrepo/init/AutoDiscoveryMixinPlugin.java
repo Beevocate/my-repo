58603d16841be141712936f5fc0c4adc3fe3948c
GitHub Action <action@github.com>
2024-06-04 15:10:47 +0000

    Initialize template repository proper
    Set package name to com.github.beevocate.myrepo
    Set project name to my-repo
    Set modid to myrepo

--- src/main/java/com/github/beevocate/myrepo/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/beevocate/myrepo/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.beevocate.myrepo.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
